c1734a68d0a1f306d69a9df4393d6d3409354565
LordalZero <lordalzero@gmail.com>
2018-04-10 02:32:13 -0500

    Added a transition to game

--- Project C Prototype/Assets/Scripts/MainMenu.cs
+++ Project C Prototype/Assets/Scripts/MainMenu.cs
@@ -2,22 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 public class MainMenu : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+	public GameObject fadedBlack;
+	private bool isStarting;
 	
-	// Update is called once per frame
+
+	void Awake() {
+		isStarting = false;
+	}
+
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.Return)){
-			SceneManager.LoadScene("Gemu");
+		if (Input.GetKeyDown(KeyCode.Return) && isStarting == false){
+			fadedBlack.SetActive(true);
+			//VolumeDownMusic();
+			Invoke("VolumeDownMusic", 5f);
+			Invoke("StartGemu", 11f);
+			isStarting = true;
 		}
 
 		if (Input.GetKeyDown(KeyCode.Escape)){
 			Application.Quit();
 		}
+	}
+
+	void StartGemu() {
+		SceneManager.LoadScene("Gemu");
+	}
+
+	void VolumeDownMusic(){
+		GameObject musicManager = GameObject.Find("MusicManager");
+		musicManager.GetComponent<AudioSource>().DOFade(0,5f);
 	}
 }
