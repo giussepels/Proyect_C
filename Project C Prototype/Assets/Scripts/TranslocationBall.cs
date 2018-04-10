5749dcc736489bd1a6998bf9ff254047a1cf0b92
LordalZero <lordalzero@gmail.com>
2018-04-09 23:49:38 -0500

    Project C Regression
    Regression to the ver 0.02

---
+++ Project C Prototype/Assets/Scripts/TranslocationBall.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TranslocationBall : MonoBehaviour {
+	public float initialForce;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody>().AddForce(transform.forward * initialForce, ForceMode.Impulse);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Project C Prototype/Assets/Scripts/TranslocationSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityStandardAssets.Utility;
+using UnityEngine;
+
+public class TranslocationSpawner : MonoBehaviour {
+	public GameObject translocationBallOject;
+	//public Camera fpsCamera;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0) && GameObject.FindWithTag("TBall") == null){
+			Instantiate(translocationBallOject, transform.position,  Camera.main.transform.rotation);
+		}
+	}
+}
